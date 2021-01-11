df09e81a01180e83fe5ca092e29c4a4f3cb3d199
samira <sami.younesy@gmail.com>
2021-01-11 09:12:13 +0100

    duplicated folder deleted

--- Reservation de chambres SOAP/server/src/main/java/test/ChambreWebServiceImpl.java
+++ Reservation de chambres SOAP/server/src/main/java/test/ChambreWebServiceImpl.java
@@ -1,6 +1,7 @@
 import javax.jws.WebService;
 import java.lang.Math;
 import java.util.Date;
+import java.util.Random;
 
  @WebService(endpointInterface="ChambreWebService", targetNamespace="http://localhost:10000/ReservationVoyage")
  public class ChambreWebServiceImpl implements ChambreWebService {
@@ -82,7 +83,7 @@ import java.util.Date;
 
     }
 
-  /*  public boolean verifierDispo(chambre c, String dateA, String dateD){
+    public boolean verifierDispo(chambre c, String dateA, String dateD){
 
       String[] dateASplit = dateA.split("_");
       String[] dateDSplit = dateD.split("_");
@@ -92,7 +93,7 @@ import java.util.Date;
       Date dateAr = new Date(Integer.parseInt(dateASplit[0]),Integer.parseInt(dateASplit[1]),Integer.parseInt(dateASplit[2]));
       Date dateDe = new Date(Integer.parseInt(dateDSplit[0]),Integer.parseInt(dateDSplit[1]),Integer.parseInt(dateDSplit[2]));
 
-      for(int i=0;i<1002;i++){
+      /*for(int i=0;i<1002;i++){
 
         //System.out.println("Num Chambre Reservee " + chambreReservee.getNumero());
 
@@ -109,9 +110,10 @@ import java.util.Date;
 
 
 
-      }
+      }*/
 
-      return dispo ;
+      Random dispo = new Random();
+      return dispo.nextBoolean();
     }
 
 
